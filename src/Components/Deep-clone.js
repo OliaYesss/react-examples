9f733aff1479f611194e8e545c6d5fde11455c1a
Olha Shekhet <oliayesss@gmail.com>
2023-04-20 17:49:44 -0700

    Add Deep-clone component

---
+++ src/Components/Deep-clone.js
@@ -0,0 +1,26 @@
+/**
+ * @param {*} value
+ * @return {*}
+ */
+export default function deepClone(value) {
+  if (Array.isArray(value)) {
+    return value.map(i => deepClone(i))
+  } else if (typeof(value) === 'object' && value !== null) {
+    return Object
+      .keys(value)
+      .reduce(
+        (memo, key) => ({...memo, [key]: deepClone(value[key])}),
+        {}
+      )
+  } else {
+    return value
+  }
+}
+
+function delay(ms) {
+  return new Promise(res => {
+    setTimeout(() => res(), ms)
+  })
+}
+
+delay(3000).then(() => alert('runs after 3 seconds'));
